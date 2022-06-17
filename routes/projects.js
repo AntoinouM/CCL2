c39913c08a19817503543c1843ee21e2c083fefc
cc211036 <cc211036@hotmail.fr>
2022-06-17 13:34:15 +0200

    beginning of routes.ejs + transfr back to front of the routes object

--- routes/projects.js
+++ routes/projects.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const authenticationService = require('../services/authentication');
+const projectsController = require('../controllers/projectsController');
 
 //router.use(authenticationService.authenticateJWT);
 
 module.exports = router;
 
-const projectsController = require('../controllers/projectsController');
--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,23 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const authenticationService = require('../services/authentication');
+const routesController = require('../controllers/routesController');
+const climbersModel = require("../models/climbersModel");
+const routesModel = require("../models/routesModel");
 
 //router.use(authenticationService.authenticateJWT);
 
+// routes display page
+router.get('/', (req, res) => {
+    routesModel.getRoutes()
+        .then(routes => {
+            res.render('routes', {routes: routes, routesJSON: JSON.stringify(routes)})
+        })
+})
+
 module.exports = router;
 
-const routesController = require('../controllers/routesController');
+
+
+
 
